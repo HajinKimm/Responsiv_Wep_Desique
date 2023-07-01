fbd120e8a9f6bb1f9bb78b060ef876d1dbbfeab1
HajinKimm <11rlagkwls@naver.com>
2023-07-01 22:32:01 +0900

    반응형웹 tablet 1023px, mobile 767px 추가

---
+++ js/main.js
@@ -0,0 +1,39 @@
+//top버튼 스크롤 맨위로
+const topBtn = document.querySelector('#footer .top')
+topBtn.addEventListener('click',e=>{
+    e.preventDefault()
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+})
+
+// 영문 대문자로 변경
+const nav = document.querySelectorAll('#header .nav .gnb li a')
+const textUpperCase =(name)=>{
+    for (var i = 0; i < name.length; i++) {
+        var text = name[i].textContent;
+        var upperCaseText = text.toUpperCase();
+        name[i].textContent = upperCaseText;
+      }
+}
+textUpperCase(nav)
+
+//태블릿창, 모바일창 메뉴버튼 토글
+const allMenu = document.querySelector('#header .all-menu i');
+const gnb = document.querySelector('#header .nav .gnb');
+const bg = document.querySelector('#header .bg');
+
+allMenu.addEventListener('click',e=>{
+    if(e.currentTarget.classList.contains('xi-bars')){
+        allMenu.classList.replace('xi-bars','xi-close');
+        gnb.style.display='block';
+        bg.style.display='block';
+    }else{
+        allMenu.classList.replace('xi-close','xi-bars')
+        gnb.style.display='none';
+        bg.style.display='none';
+    }
+})
+bg.addEventListener('click',e=>{
+    allMenu.classList.replace('xi-close','xi-bars')
+    gnb.style.display='none';
+    bg.style.display='none';
+})
--- js/swiper.js
+++ js/swiper.js
@@ -1,22 +1,79 @@
 var swiper = new Swiper(".myVisual", {
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    autoplay: {
-        delay: 3000,
-      },
-      loop:true
-    
-  });
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+    delay: 3000,
+  },
+  loop: true
 
+});
 
-  var swiper = new Swiper(".myMainCollect", {
-    pagination: {
-      el: ".swiper-pagination",
+var swiper = new Swiper(".myBestSellers", {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  loop: true,
+  breakpoints: {
+    0: {
+        slidesPerView: 2, //화면에 보이는 이미지 갯수
+        spaceBetween: 30, //이미지사이의 여백
     },
-    autoplay: {
-      delay: 3000,
+    500: {
+        slidesPerView: 2.5,
+        spaceBetween: 30,
     },
-    loop:true
-  });
+    768: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+    },
+    1024: {
+        slidesPerView: 5,
+        spaceBetween: 30,
+    },
+  },
+});
+
+var swiper = new Swiper(".myMainCollect", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  autoplay: {
+    delay: 3000,
+  },
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".myMainReview", {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  loop: true,
+  breakpoints: {
+    0: {
+        slidesPerView: 2, //화면에 보이는 이미지 갯수
+        spaceBetween: 30, //이미지사이의 여백
+    },
+    500: {
+      slidesPerView: 2.5,
+      spaceBetween: 30,
+    },
+    768: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+    },
+    1024: {
+        slidesPerView: 5,
+        spaceBetween: 30,
+    },
+  },
+});
